2bfd298322b8a700019d8e3e505b853385544e7d
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-09-10 15:08:01 +0200

    Ajout commentaire pour expliquer choix pour Address

--- models/address.py
+++ models/address.py
@@ -15,6 +15,7 @@ class Address:
     street: str
     city: str
     postal_code: str
-
+    # Modif postal_code en string au lieu de int
+    # Modif dans MySQL passage en VARCHAR(5) au lieu de SMALLINT trop petit et pb avec cp en 0
     def __str__(self) -> str:
         return f"{self.street}, {self.postal_code} {self.city}"
